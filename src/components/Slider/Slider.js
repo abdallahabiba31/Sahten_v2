93c25f549cc286a42fdca49e3fbae38449071275
abdallahabiba31 <abdallahabiba97@gmail.com>
2023-06-27 02:01:31 +0200

    - second Api with joke

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import { useEffect, useState } from "react";
 import './Slider.css'
+import axios from "axios";
 
 const Slider = () => {
+  const [joke, setJoke] = useState('');
+  const [punchline, setPunchline] = useState('');
+
+    useEffect(() => {
+      axios.get(`https://official-joke-api.appspot.com/random_joke`).then(response => {
+        console.log(response.data.setup);
+        console.log(response.data.punchline);
+        setJoke(response.data.setup);
+        setPunchline(response.data.punchline);
+      })
+       
+    }, [])
   return (
     <div className='container slider-box'>
-        
+        <h1 className="jokeText">{joke}</h1>
+        <h3 className="punchText">{punchline}</h3>
     </div>
   )
 }
